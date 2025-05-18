cd586c83c8a9f890d779b477487edef9aa65c760
tatanecochink <charles.alexandre.deschenes@gmail.com>
2025-05-18 19:35:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Bot de trading automatique lancé avec succès.")
+import requests
+
+def lire_nouvelle():
+    url = "https://newsapi.org/v2/top-headlines"
+    params = {
+        "country": "ca",
+        "apiKey": "INSÈRE_TA_CLÉ_API_ICI"
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        data = response.json()
+        titre = data["articles"][0]["title"]
+        print("Nouvelle principale :", titre)
+    else:
+        print("Erreur lors du chargement des nouvelles :", response.status_code)
+
+lire_nouvelle()
