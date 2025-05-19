8005cc225ab644703b4066c5a4afa22540806b9c
tatanecochink <charles.alexandre.deschenes@gmail.com>
2025-05-18 20:20:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import requests
 
 def lire_nouvelle():
- url = "https://newsapi.org/v2/everything"
+    url = "https://newsapi.org/v2/everything"
     params = {
-        "country": "us",
-        "category": "business",
+        "q": "économie",
         "apiKey": "6811ae98a9554ae490eae22d1ad9ca37"
+    }
+
     response = requests.get(url, params=params)
 
     if response.status_code == 200:
         data = response.json()
-        articles = data.get("articles", [])
-        if articles:
-            titre = articles[0]["title"]
+        if data["articles"]:
+            titre = data["articles"][0]["title"]
             print("Nouvelle principale :", titre)
         else:
             print("Aucune nouvelle trouvée.")
